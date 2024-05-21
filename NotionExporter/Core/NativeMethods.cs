5fdcb65ebcbdd7f88db12bae4eb2d8c88e4ec6ac
Ryan Bester <ryan.bester5@gmail.com>
2024-05-21 22:31:54 +0100

    feat(ui): add custom save dialog

--- NotionExporter/Core/NativeMethods.cs
+++ NotionExporter/Core/NativeMethods.cs
@@ -1,17 +1,90 @@
 ﻿using System.Runtime.InteropServices;
+using System.Text;
 
 namespace NotionExporter.Core;
 
 public static class NativeMethods
 {
+    public const int WM_SYSCOMMAND = 0x112;
+    public const int MF_SEPARATOR = 0x800;
+    public const int MF_BYPOSITION = 0x400;
+    public const int MF_STRING = 0x0;
+
+    public const int EVENT_OBJECT_SHOW = 0x8002;
+    public const int EVENT_OBJECT_LOCATIONCHANGE = 0x800B;
+    public const int EVENT_OBJECT_NAMECHANGE = 0x800C;
+
+    public const uint WINEVENT_OUTOFCONTEXT = 0;
+
+    public delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild,
+        uint dwEventThread, uint dwmsEventTime);
+
+    public delegate bool EnumChildProc(IntPtr hWnd, IntPtr lParam);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+    }
+
     [DllImport("user32.dll")]
     public static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
 
     [DllImport("user32.dll")]
-    public static extern bool InsertMenu(IntPtr hMenu, Int32 wPosition, Int32 wFlags, Int32 wIDNewItem, string lpNewItem);
-    
-    public const Int32 WM_SYSCOMMAND = 0x112;
-    public const Int32 MF_SEPARATOR = 0x800;
-    public const Int32 MF_BYPOSITION = 0x400;
-    public const Int32 MF_STRING = 0x0;
+    public static extern bool InsertMenu(IntPtr hMenu, int wPosition, int wFlags, int wIDNewItem,
+        string lpNewItem);
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc,
+        WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+    [DllImport("user32.dll")]
+    public static extern bool UnhookWinEvent(IntPtr hWinEventHook);
+
+    [DllImport("user32.dll")]
+    public static extern bool MoveWindow(IntPtr hWnd, int x, int y, int w, int h, bool repaint);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass,
+        string lpszWindow);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool EnumChildWindows(IntPtr hWndParent, EnumChildProc lpEnumFunc, IntPtr lParam);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+    [DllImport("user32.dll")]
+    public static extern bool GetClientRect(IntPtr hWnd, out RECT rc);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern IntPtr GetParent(IntPtr hWnd);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern IntPtr SetParent(IntPtr hwndChild, IntPtr hwndNewParent);
+
+    [DllImport("user32.dll")]
+    static extern IntPtr GetDC(IntPtr hWnd);
+
+    [DllImport("user32.dll")]
+    static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+    [DllImport("gdi32.dll")]
+    static extern uint GetPixel(IntPtr hdc, int nXPos, int nYPos);
+
+    public static Color GetControlBackgroundColor(IntPtr hWnd, Point location)
+    {
+        var hdc = GetDC(hWnd);
+        var pixel = GetPixel(hdc, location.X, location.Y);
+        ReleaseDC(hWnd, hdc);
+
+        Color color = Color.FromArgb((int)(pixel & 0x000000FF),
+            (int)((pixel & 0x0000FF00) >> 8),
+            (int)((pixel & 0x00FF0000) >> 16));
+
+        return color;
+    }
 }
---
+++ NotionExporter/UI/Controls/CustomSaveDialog.cs
@@ -0,0 +1,108 @@
+﻿using System.Text;
+using static NotionExporter.Core.NativeMethods;
+
+namespace NotionExporter.UI.Controls;
+
+public class CustomSaveDialog : IDisposable
+{
+    private readonly IntPtr _hHook;
+    private readonly IntPtr _hCtrl;
+    private readonly UserControl _ctrl;
+
+    private IntPtr _hSfd;
+
+    private static readonly WinEventDelegate ProcDelegate = WinEventProc;
+    private static CustomSaveDialog? _staticSfd;
+    private static UserControl? _staticCtrl;
+    private static IntPtr _hWndBottomBar = IntPtr.Zero;
+    private static Color _color = Color.White;
+
+    public SaveFileDialog Dlg { get; }
+
+    public CustomSaveDialog(SaveFileDialog dlg, UserControl ctrl)
+    {
+        Dlg = dlg;
+        _ctrl = ctrl;
+        _staticSfd = this;
+        _hCtrl = ctrl.Handle;
+        _staticCtrl = ctrl;
+
+        _hHook = SetWinEventHook(EVENT_OBJECT_SHOW, EVENT_OBJECT_NAMECHANGE, IntPtr.Zero, ProcDelegate,
+            (uint)Environment.ProcessId, 0, WINEVENT_OUTOFCONTEXT);
+    }
+
+    private static bool EnumChildCallback(IntPtr hWnd, IntPtr lParam)
+    {
+        var className = new StringBuilder(256);
+        GetClassName(hWnd, className, className.Capacity);
+
+        if (className.ToString() == "ToolbarWindow32")
+        {
+            _hWndBottomBar = hWnd;
+            return false;
+        }
+
+        return true;
+    }
+
+    private static void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hWnd, int idObject, int idChild,
+        uint dwEventThread, uint dwmsEventTime)
+    {
+        if (_staticSfd != null)
+        {
+            if (_staticSfd?._hSfd == IntPtr.Zero)
+            {
+                _staticSfd._hSfd = FindWindowEx(IntPtr.Zero, IntPtr.Zero, "#32770", _staticSfd.Dlg.Title);
+            }
+
+            if (eventType == EVENT_OBJECT_SHOW)
+            {
+                if (_hWndBottomBar == IntPtr.Zero && _staticSfd != null)
+                {
+                    EnumChildWindows(_staticSfd._hSfd, EnumChildCallback, IntPtr.Zero);
+                }
+            }
+
+            if (_staticSfd != null && hWnd == _staticSfd._hSfd && eventType is EVENT_OBJECT_NAMECHANGE
+                    or EVENT_OBJECT_LOCATIONCHANGE)
+            {
+                var hParent = GetParent(_staticSfd._hCtrl);
+
+                if (hParent != _staticSfd._hSfd)
+                {
+                    SetParent(_staticSfd._hCtrl, _staticSfd._hSfd);
+                }
+
+                GetClientRect(_staticSfd._hSfd, out var cliRect);
+                MoveWindow(_staticSfd._hCtrl, cliRect.Left + 130, cliRect.Bottom - 55, 500, 60, true);
+            }
+        }
+
+        if (_staticCtrl != null && hWnd == _hWndBottomBar && eventType == EVENT_OBJECT_LOCATIONCHANGE)
+        {
+            _color = GetControlBackgroundColor(_hWndBottomBar, new Point(2, 2));
+            _staticCtrl.BackColor = _color;
+            _staticCtrl.Invalidate();
+        }
+    }
+
+    private void Dispose(bool disposing)
+    {
+        UnhookWinEvent(_hHook);
+        _hWndBottomBar = IntPtr.Zero;
+        _staticSfd = null;
+        _staticCtrl = null;
+
+        if (disposing)
+        {
+            Dlg.Dispose();
+            _ctrl.Dispose();
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
